44972f723b80e41e0616cba10ef23823c157f2d2
Arman Alam <armanalam78578@gmail.com>
2026-03-25 19:45:24 +0530

    product searchlist backend completed

--- components/client/product-search-result.tsx
+++ components/client/product-search-result.tsx
@@ -1,36 +1,73 @@
 import { CornerDownRightIcon } from "lucide-react";
 import { Separator } from "../ui/separator";
+import { useSearchParams } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
+import { searchProducts } from "../server/getSearchResult.action";
+import { formUrlQuery } from "@/lib/utils";
 
+interface searchProp {
+  title: string;
+  category: string;
+  id: string;
+}
 const ProductSearchResult = () => {
+  const searchParams = useSearchParams();
+  const [searchList, setSearchList] = useState<searchProp[]>([]);
+  const [isAdmin, setIsAdmin] = useState<boolean>(false);
+  const type = searchParams.get("type");
+  const searchQuery = searchParams.get("q") ?? "";
+
+  const handleSearchProducts = useCallback(async () => {
+    const fetchedProducts = await searchProducts({ query: searchQuery, type });
+    setSearchList(JSON.parse(fetchedProducts));
+  }, [searchQuery]);
+
+  useEffect(() => {
+    handleSearchProducts();
+  }, [handleSearchProducts]);
+
   return (
     <div className="overflow-y-auto pb-2 w-full">
       <div className="flex px-2 justify-end text-xs items-center gap-x-1">
-        <span>Results:</span>25{" "}
+        <span>Results:</span> {searchQuery ? searchList.length : 0}
       </div>
-      <Separator />
-      <div className="flex flex-col gap-y-1.5 mt-2">
-        {Array.from({ length: 3 }).map((_, i) => (
-          <div
-            key={i}
-            onClick={()=> console.log(i)}
-            className="flex text-sm cursor-pointer px-2 line-clamp-1 rounded gap-x-2 items-center"
-          >
-            <CornerDownRightIcon size={15} /> arman is a good boy
+      {searchQuery ? (
+        <>
+          <Separator />
+          <div className="flex flex-col gap-y-1.5 mt-2">
+            {searchList.length > 0 ? (
+              searchList.map((list, i) => (
+                <div
+                  key={i}
+                  onClick={() => console.log(i)}
+                  className="flex text-sm cursor-pointer px-2 line-clamp-1 rounded gap-x-2 items-center"
+                >
+                  <CornerDownRightIcon size={15} /> {list.title}
+                </div>
+              ))
+            ) : (
+              <p className="text-sm font-semibold">No results</p>
+            )}
           </div>
-        ))}
-      </div>
-      <Separator className="mt-2" />
-      <p className="text-xs mt-2 mb-1 px-2 font-semibold">Suggesstion:</p>
-      <div className="flex gap-y-1.5 flex-col">
-        {Array.from({ length: 3 }).map((_, i) => (
-          <div
-            key={i}
-            className="flex text-sm cursor-pointer px-2 rounded line-clamp-1 gap-x-2 items-center"
-          >
-            <CornerDownRightIcon size={15} /> arman is a good boy
+        </>
+      ) : (
+        <>
+          <Separator className="mt-2" />
+          <p className="text-xs mt-2 mb-1 px-2 font-semibold">Suggesstion:</p>
+          <div className="flex gap-y-1.5 flex-col">
+            {searchList.length > 0 &&
+              searchList.map((list, i) => (
+                <div
+                  key={i}
+                  onClick={() => console.log(i)}
+                  className="flex text-sm cursor-pointer px-2 line-clamp-1 rounded gap-x-2 items-center"
+                >
+                  <CornerDownRightIcon size={15} /> {list.title}
+                </div>
+              ))}
           </div>
-        ))}
-      </div>
+        </>
+      )}
     </div>
   );
 };
